8c2ccaf103b6d399b29243006fb232f5a06942fd
ArhanDevelops <167398702+ArhanDevelops@users.noreply.github.com>
2024-04-19 18:51:35 +0600

    Create loader.lua

---
+++ src/loader.lua
@@ -0,0 +1,8 @@
+-- lalpha is 13694267880
+-- bin2 is 13864661000
+
+if game.PlaceId == 13864661000 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ArhanDevelops/belugax/main/src/bin2.lua"))()
+elseif game.PlaceId = 13694267880
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ArhanDevelops/belugax/main/src/lalpha.lua"))()
+end
