353bffb9d3c7cce28ed1e15283e95ad2640843fa
ArhanDevelops <167398702+ArhanDevelops@users.noreply.github.com>
2024-04-20 13:10:25 +0600

    Update testing.lua

--- src/testing.lua
+++ src/testing.lua
@@ -1,6 +1,6 @@
 -- for all those script kiddies, i dont test the game scripts i test the features here.
 
-if _G.Accepted = true then
+if _G.Accepted == true then
   game.Players.LocalPlayer:Kick("chat it worked...")
 else
   error("bruh")
