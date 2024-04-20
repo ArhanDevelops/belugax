ea6b88abd1dff315f58047bf263a03c4bc07413b
ArhanDevelops <167398702+ArhanDevelops@users.noreply.github.com>
2024-04-20 13:02:14 +0600

    Create testing.lua

---
+++ src/testing.lua
@@ -0,0 +1,7 @@
+-- for all those script kiddies, i dont test the game scripts i test the features here.
+
+if _G.Accepted = true then
+  game.Players.LocalPlayer:Kick("chat it worked...")
+else
+  error("bruh")
+end
