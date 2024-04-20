116a70836f5f680c537cfef5c3538982ee12c4ce
ArhanDevelops <167398702+ArhanDevelops@users.noreply.github.com>
2024-04-20 13:06:00 +0600

    Update loader.lua

--- src/loader.lua
+++ src/loader.lua
@@ -1,8 +1,18 @@
--- lalpha is 13694267880
--- bin2 is 13864661000
+-- KEY SYSTEM V2 UI LIBRARY:
+-- UI by mr.xrer | Code by mstudio45
 
-if game.PlaceId == 13864661000 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ArhanDevelops/belugax/main/src/bin2.lua"))()
-elseif game.PlaceId = 13694267880
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ArhanDevelops/belugax/main/src/lalpha.lua"))()
+local KeySystemUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaGiXxScripter0/keysystemv2api/master/ui/xrer_mstudio45.lua"))()
+KeySystemUI.New({
+    ApplicationName = "BelugaHub", -- Your Key System Application Name
+    Name = "Beluga X Hub", -- Your Script name
+    Info = "Enter your key here.", -- Info text in the GUI, keep empty for default text.
+    DiscordInvite = "", -- Optional.
+    AuthType = "clientid" -- Can select verification with ClientId or IP ("clientid" or "ip")
+})
+repeat task.wait() until KeySystemUI.Finished() or KeySystemUI.Closed
+if KeySystemUI.Finished() and KeySystemUI.Closed == false then
+    _G.Accepted = true
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ArhanDevelops/belugax/main/src/testing.lua"))()
+else
+    print("Player closed the GUI.")
 end
